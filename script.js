b5c2c0d9a2445b06b352910ac8de7c779cc090f7
chipsandpudding <78986697+chipsandpudding@users.noreply.github.com>
2021-02-12 17:52:42 -0500

    added index and script

---
+++ script.js
@@ -0,0 +1,11 @@
+var firstName = prompt("What is your first name?");
+
+var favColor = prompt("What is your favorite color");
+
+document.body.querySelector("#box1").innerHTML="My name is";
+
+document.body.querySelector("#box2").innerHTML=firstName;
+
+document.body.querySelector("#box3").innerHTML="and my favorite color is";
+
+document.body.querySelector("#box4").innerHTML=favColor;
